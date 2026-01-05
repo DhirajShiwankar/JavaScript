a563e5c117ed314fb6cba91b6d8a133b3987bed8
DhirajShiwankar <dhirajshivankar90.gmail.com>
2026-01-05 08:30:10 +0530

    update

---
+++ Basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 12345;
+let email = "dhiraj@gmail.com"
+var Password = "Nagpur123"
+accountcity = "Nagpur"
+let accountStat;
+
+// accountId = 2; // this is not allow because it is constant
+email = "dhiraj1@gmail.com"
+Password = "Nagpur1234"
+accountcity = "pune"
+
+console.log(accountId);
+console.table([accountId, Password, email, accountcity, accountStat])
---
+++ Basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello"); //we are using node JS not browser
+
+let name = "Dhiraj";
+let num = 22;
+let value = true;
+let state;
+
+// number => 2 to power 53
+// BigInt
+// String = ""
+// Boolean = true / false
+// null => Standalone value
+// undefined
+// Symbol => unique
+// object
+
+console.log(typeof "Dhiraj");
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
