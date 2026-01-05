670c27ea917bf095760545052f897d13f1782b5d
DhirajShiwankar <dhirajshivankar90.gmail.com>
2026-01-05 15:36:55 +0530

    Update file

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,36 @@
+const arr = [0, 1, 2, 3, 4, 5];
+const arr2 = ["Shakiman", "Nagraaj"];
+const arr3 =new Array(1, 2, 3, 4)
+// console.log(arr[1]);
+
+// Array methods
+
+// arr.push(6);
+// arr.push(7);
+// arr.pop()
+
+// arr.unshift(9) // starting add value
+// arr.shift() // starting value remove
+
+// console.log(arr.includes(9));
+// console.log(arr.indexOf());
+
+const newarr = arr.join()
+
+// console.log(arr);
+// console.log(newar
+
+console.log("A ", arr);
+const myArr = arr.slice(1, 3)
+console.log(myArr);
+console.log("B ", arr);
+
+const myArr1 = arr.splice(1, 3)
+console.log(myArr1);
+console.log("c ", arr);
+
+
+
+
+
+
